7639e11c9511955be36151fe85510e68a6883d38
Cameron Lowe <cam.m.lowe@gmail.com>
2015-10-28 11:47:47 -0500

    Test OneToOneRelationship

--- parent_swap/migrations.py
+++ parent_swap/migrations.py
@@ -1,3 +1,5 @@
+from django.db import models
+
 from .swap import _get_class_object
 
 
@@ -10,5 +12,29 @@ def get_cls_ptr(cls):
     return '{}_ptr'.format(cls._meta.object_name.lower())
 
 
+def get_app_reference(cls):
+    """
+    Return simplified App Model reference e.g., app_label.ModelName
+    """
+    return '{0}.{1}'.format(cls._meta.app_label, cls._meta.object_name)
+
+
+def get_one_to_one_field_config(cls):
+    """
+    Returns the models.OneToOneField necessary for the migration file.
+    Parent Class specific currently.
+    """
+    if isinstance(cls, str):
+        cls = _get_class_object(cls)
+    app_reference = get_app_reference(cls)
+    return models.OneToOneField(
+        parent_link=True, auto_created=True, primary_key=True, serialize=False, to=app_reference
+    )
+
+
 def get_swap_field(cls):
-    pass
+    """
+    Returns the appropriately configured field for a swapped model in a migration
+    """
+    ptr = get_cls_ptr(cls)
+    return (ptr,)
--- parent_swap/tests/test_migrations.py
+++ parent_swap/tests/test_migrations.py
@@ -1,15 +1,53 @@
-from parent_app import models
+from django.db import models
 
-from ..migrations import get_cls_ptr
+from parent_app.models import SimpleParent
+
+from ..migrations import get_cls_ptr, get_swap_field, get_one_to_one_field_config
 from .utils import AppReloadTestCase
 
 
 class TestMigrationSwap(AppReloadTestCase):
 
     def test_get_cls_ptr(self):
-        ptr = get_cls_ptr(models.SimpleParent)
+        ptr = get_cls_ptr(SimpleParent)
         self.assertEquals(ptr, 'simpleparent_ptr')
 
     def test_get_cls_ptr_string(self):
         ptr = get_cls_ptr('parent_swap.tests.parent_app.models.SimpleParent')
         self.assertEquals(ptr, 'simpleparent_ptr')
+
+    def test_get_swap_field(self):
+        field = get_swap_field(SimpleParent)
+        self.assertEqual(field, ('simpleparent_ptr',))
+
+    def test_get_swap_field_str(self):
+        field = get_swap_field('parent_swap.tests.parent_app.models.SimpleParent')
+        self.assertEqual(field, ('simpleparent_ptr',))
+
+    def test_get_one_to_one_field_config(self):
+        field = get_one_to_one_field_config(SimpleParent)
+        expected_field = models.OneToOneField(
+            parent_link=True,
+            auto_created=True,
+            primary_key=True,
+            serialize=False,
+            to='parent_app.SimpleParent'
+        )
+        self.assertEqual(field.auto_created, expected_field.auto_created)
+        self.assertEqual(field.primary_key, expected_field.primary_key)
+        self.assertEqual(field.serialize, expected_field.serialize)
+        self.assertEqual(field.related_model, 'parent_app.SimpleParent')
+
+    def test_get_one_to_one_field_config_str(self):
+        field = get_one_to_one_field_config('parent_swap.tests.parent_app.models.SimpleParent')
+        expected_field = models.OneToOneField(
+            parent_link=True,
+            auto_created=True,
+            primary_key=True,
+            serialize=False,
+            to='parent_app.SimpleParent'
+        )
+        self.assertEqual(field.auto_created, expected_field.auto_created)
+        self.assertEqual(field.primary_key, expected_field.primary_key)
+        self.assertEqual(field.serialize, expected_field.serialize)
+        self.assertEqual(field.related_model, 'parent_app.SimpleParent')
