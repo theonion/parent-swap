1b9bba53c140d12396af3337b1e874f6225585db
Cameron Lowe <cam.m.lowe@gmail.com>
2015-10-28 13:48:51 -0500

    modular model/string validation

--- parent_swap/fields.py
+++ parent_swap/fields.py
@@ -4,21 +4,24 @@ from django.db import models
 from .swap import _get_class_object
 
 
-def get_model_from_str(cls_name):
-    if not isinstance(cls_name, str):
-        raise TypeError("The provided class name must be in a string format")
-    try:
-        return apps.get_model(cls_name)
-    except:
-        return _get_class_object(cls_name)
+def validate_or_get_model(cls):
+    """
+    Returns the class iff the reference to the model is string/unicode
+    """
+    if isinstance(cls, str) or isinstance(cls, unicode):
+        try:
+            return apps.get_model(cls)
+        except:
+            return _get_class_object(cls)
+    else:
+        return cls
 
 
 def get_cls_ptr(cls):
     """
     Returns the name of the class pointer for a given OneToOne relationship.
     """
-    if isinstance(cls, str):
-        cls = get_model_from_str(cls)
+    cls = validate_or_get_model(cls)
     return '{}_ptr'.format(cls._meta.object_name.lower())
 
 
@@ -34,8 +37,7 @@ def get_one_to_one_field_config(cls):
     Returns the models.OneToOneField necessary for the migration file.
     Parent Class specific currently.
     """
-    if isinstance(cls, str):
-        cls = get_model_from_str(cls)
+    cls = validate_or_get_model(cls)
     app_reference = get_app_reference(cls)
     return models.OneToOneField(
         parent_link=True, auto_created=True, primary_key=True, serialize=False, to=app_reference
@@ -46,8 +48,7 @@ def get_swap_field(cls):
     """
     Returns the appropriately configured field for a swapped model in a migration
     """
-    if isinstance(cls, str):
-        cls = get_model_from_str(cls)
+    cls = validate_or_get_model(cls)
     ptr = get_cls_ptr(cls)
     rel = get_one_to_one_field_config(cls)
     return (ptr, rel)
