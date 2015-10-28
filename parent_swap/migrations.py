d7bf7b8a15b3f48c26d748089a7ba8b14b59c33a
Cameron Lowe <cam.m.lowe@gmail.com>
2015-10-28 10:52:52 -0500

    test for string argument

--- parent_swap/migrations.py
+++ parent_swap/migrations.py
@@ -1,6 +1,14 @@
+from .swap import _get_class_object
+
 
 def get_cls_ptr(cls):
     """
     Returns the name of the class pointer for a given OneToOne relationship.
     """
+    if isinstance(cls, str):
+        cls = _get_class_object(cls)
     return '{}_ptr'.format(cls._meta.object_name.lower())
+
+
+def get_swap_field(cls):
+    pass
--- parent_swap/tests/test_migrations.py
+++ parent_swap/tests/test_migrations.py
@@ -9,3 +9,7 @@ class TestMigrationSwap(AppReloadTestCase):
     def test_get_cls_ptr(self):
         ptr = get_cls_ptr(models.SimpleParent)
         self.assertEquals(ptr, 'simpleparent_ptr')
+
+    def test_get_cls_ptr_string(self):
+        ptr = get_cls_ptr('parent_swap.tests.parent_app.models.SimpleParent')
+        self.assertEquals(ptr, 'simpleparent_ptr')
